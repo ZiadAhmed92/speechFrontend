485929c809bda83702fedd53c75b5768d17785c4
ZiadAhmed92 <za693387@gmail.com>
2024-01-18 21:22:14 +0200

    test

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,9 @@ import './App.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap/dist/js/bootstrap.bundle.min'
 // import '@fortawesome/fontawesome-free/css/all.min.css'
-import Welcome1 from './Components/Welcome/welcome1'
-import Welcome2 from './Components/Welcome/welcome2'
-import Welcome3 from './Components/Welcome/welcome3'
+import Welcome1 from './Components/Welcome/Welcome1'
+import Welcome2 from './Components/Welcome/Welcome2'
+import Welcome3 from './Components/Welcome/Welcome3'
 import { RouterProvider, createBrowserRouter } from 'react-router-dom'
 function App() {
  
