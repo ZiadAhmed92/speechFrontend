c5b41d2c726e35a11cc5e7a822ba1edc733352a5
ZiadAhmed92 <za693387@gmail.com>
2024-01-25 21:54:16 +0200

    test

--- src/Components/Welcome/WelcomeThird.jsx
+++ src/Components/Welcome/WelcomeThird.jsx
@@ -4,16 +4,18 @@ import "./Welcome.css"
 const WelcomeThird = () => {
   return (
       <div className="welcome1">
-          <div className="text-center">
-              <img src={logo3} className="logo1" />
-          </div>
-          <div className="paragraph-welcome1 w-75 m-auto">
-           
-              <h6 className="text-wel2 text-center"> <span className="sub-title">Then ,</span> Emotion statistics are shown in their percentages by analyzing the voice.</h6>
-          </div>
-          <div className="w-75 text-center">
-              <Link to="/register"> <button className="btn-welcome1">Get Started</button></Link>
-          </div>
+        <div className=" text-center">
+            <div className="text-center">
+                <img src={logo3} className="logo1" />
+            </div>
+            <div className="paragraph-welcome1 w-75 m-auto">
+             
+                <h6 className="text-wel2 text-center"> <span className="sub-title">Then ,</span> Emotion statistics are shown in their percentages by analyzing the voice.</h6>
+            </div>
+            <div className="text-center">
+                <Link to="/login"> <button className="btn-welcome1">Get Started</button></Link>
+            </div>
+        </div>
       </div>
   )
 }
